87f20e9d528261ace5abff2f23fc6d890d0c1c55
Rodrigo <rodrigo@crowdbotics.com>
2022-10-08 18:32:09 +0000

    Add Blank Screen 17

---
+++ screens/BlankScreen17/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen17 = () => {
+  return <View style={_styles.BlankScreen17}>
+      </View>;
+};
+
+export default BlankScreen17;
+
+const _styles = StyleSheet.create({
+  BlankScreen17: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
