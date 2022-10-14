8a49e988ef0d2664a082389be9a91cd431264aa3
Rodrigo <rodrigo@crowdbotics.com>
2022-10-14 23:23:17 +0000

    Add Blank Screen 23

---
+++ screens/BlankScreen23/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen23 = () => {
+  return <View style={_styles.BlankScreen23}>
+      </View>;
+};
+
+export default BlankScreen23;
+
+const _styles = StyleSheet.create({
+  BlankScreen23: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
